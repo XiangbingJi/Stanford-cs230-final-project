2658cb8c0acca199a3e214511a34b0fed3ebbf97
weouyang <weouyang@eng.ucsd.edu>
2019-06-05 22:04:37 -0700

    Add shuffle data script

---
+++ data/shuffle_data.py
@@ -0,0 +1,91 @@
+import os
+
+import random
+import shutil
+
+ORIGINAL_ROOT_DIR = "/Users/loujunjie/Desktop/processed_tu_simple_data/original_image/"
+ORIGINAL_TRAIN_DIR = "/Users/loujunjie/Desktop/processed_tu_simple_data/original_train/"
+ORIGINAL_DEV_DIR = "/Users/loujunjie/Desktop/processed_tu_simple_data/original_dev/"
+ORIGINAL_TEST_DIR = "/Users/loujunjie/Desktop/processed_tu_simple_data/original_test/"
+LABEL_ROOT_DIR = "/Users/loujunjie/Desktop/processed_tu_simple_data/label_image/"
+LABEL_TRAIN_DIR = "/Users/loujunjie/Desktop/processed_tu_simple_data/label_train/"
+LABEL_DEV_DIR = "/Users/loujunjie/Desktop/processed_tu_simple_data/label_dev/"
+LABEL_TEST_DIR = "/Users/loujunjie/Desktop/processed_tu_simple_data/label_test/"
+
+def loop_directory_and_find_images(directory):
+    test_data = []
+    rest = []
+    for filename in os.listdir(directory):
+        if os.path.isdir(os.path.join(directory, filename)):
+            loop_directory(os.path.join(directory, filename))
+        elif filename.endswith(".jpg"):
+            if (filename.startswith("clips_0313-1")):
+                no_suffix = filename.split('.')[0]
+                frame = no_suffix.split('_')[2]
+                if (int(frame) / 60 % 10 == 0):
+                    test_data.append(filename)
+#                     print filename
+                else:
+                    rest.append(filename)
+            elif (filename.startswith("clips_0313-2")):
+                no_suffix = filename.split('.')[0]
+                frame = no_suffix.split('_')[2]
+                if (frame <= 1800):
+                    if (int(frame) / 5 % 10 == 0):
+#                         print filename
+                        test_data.append(filename)
+                    else:
+                        rest.append(filename)
+                else:
+                    if (int(frame) / 60 % 10 == 0):
+#                         print filename
+                        test_data.append(filename)
+                    else:
+                        rest.append(filename)
+
+    
+    return test_data, rest
+
+def loop_directory_and_copy_images(directory, new_directory):
+    for filename in os.listdir(directory):
+        if os.path.isdir(os.path.join(directory, filename)):
+            loop_directory(os.path.join(directory, filename))
+        elif filename in test_data:
+            shutil.copy2(os.path.join(directory, filename), new_directory)
+            print("Copied file ", filename, " to new directory ", new_directory)
+            
+def create_directory_if_not_exists(directory):
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+        print("Directory " , directory ,  " Created ")
+    else:    
+        print("Directory " , directory ,  " already exists")
+        
+if __name__ == '__main__':
+    test_data, rest = loop_directory_and_find_images(ORIGINAL_ROOT_DIR)
+    print len(test_data)
+    print len(rest)
+
+    SEED = 1024
+    random.seed(SEED)
+    random.shuffle(rest)
+    dev_data = rest[:281]
+    train_data = rest[281:]
+    
+    create_directory_if_not_exists(ORIGINAL_TRAIN_DIR)
+    create_directory_if_not_exists(ORIGINAL_DEV_DIR)
+    create_directory_if_not_exists(ORIGINAL_TEST_DIR)
+    
+    loop_directory_and_copy_images(ORIGINAL_ROOT_DIR, ORIGINAL_TRAIN_DIR)
+    loop_directory_and_copy_images(ORIGINAL_ROOT_DIR, ORIGINAL_DEV_DIR)
+    loop_directory_and_copy_images(ORIGINAL_ROOT_DIR, ORIGINAL_TEST_DIR)
+    
+    create_directory_if_not_exists(LABEL_TRAIN_DIR)
+    create_directory_if_not_exists(LABEL_DEV_DIR)
+    create_directory_if_not_exists(LABEL_TEST_DIR)
+    
+    loop_directory_and_copy_images(LABEL_ROOT_DIR, LABEL_TRAIN_DIR)
+    loop_directory_and_copy_images(LABEL_ROOT_DIR, LABEL_DEV_DIR)
+    loop_directory_and_copy_images(LABEL_ROOT_DIR, LABEL_TEST_DIR)
+    
+    

422975f82ebfe6ec8ce8d0a3f880b37690742261
Zac Wu <wukey1992@gmail.com>
2019-06-06 21:14:42 -0700

    Enhanced the script to generate overlay images.

--- utils/overlay.py
+++ utils/overlay.py
@@ -1,18 +1,34 @@
 from PIL import Image
 import glob
+import argparse
+import natsort
+import os.path as path
 
-original_images = []
 
-ori_glob = glob.glob('frames/*.jpg')
-mark_glob = glob.glob('predicted_frames/*.jpg')
+def generate_overlay(background_path, overlay_path, output_path):
 
-for i in range(len(ori_glob)):
-    background = Image.open(ori_glob[i])
-    overlay = Image.open(mark_glob[i])
+    ori_glob = glob.glob(background_path + '/*.jpg') + glob.glob(background_path + '/*.png')
+    mark_glob = glob.glob(overlay_path + '/*.jpg') + glob.glob(overlay_path + '/*.png')
 
-    background = background.convert("RGBA")
-    overlay = overlay.convert("RGBA")
+    print(len(ori_glob), len(mark_glob))
 
-    new_img = Image.blend(background, overlay, 0.5)
-    new_img.save("overlay/" + str(i) + ".png", "PNG")
+    for i in range(len(ori_glob)):
+        background = Image.open(ori_glob[i])
+        overlay = Image.open(mark_glob[i])
 
+        background = background.convert("RGBA")
+        overlay = overlay.convert("RGBA")
+
+        new_img = Image.blend(background, overlay, 0.5)
+        print("saving to " + output_path + " as " + path.basename(ori_glob[i]))
+        new_img.save(path.join(output_path, path.basename(ori_glob[i])), "PNG")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--background_path", type=str, default="frames")
+    parser.add_argument("--overlay_path", type=str, default="predicted_frames")
+    parser.add_argument("--output_path", type=str, default="overlay")
+
+    args = parser.parse_args()
+
+    generate_overlay(args.background_path, args.overlay_path, args.output_path)
